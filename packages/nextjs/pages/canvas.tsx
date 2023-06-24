020ca45e18ea4811bbbf0125604de45fc287d010
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2023-06-24 17:57:16 -0400

    fix: use grid display for canvas
    - chore: remove pixi

--- packages/nextjs/pages/canvas.tsx
+++ packages/nextjs/pages/canvas.tsx
@@ -1,7 +1,26 @@
-import React, { useEffect, useRef, useState } from "react";
-import * as PIXI from "pixi.js";
+import React, { useMemo, useRef, useState } from "react";
 
-const CanvasComponent: React.FC = () => {
+interface PixelMetadata {
+  color: string;
+}
+
+function create2DArray(rows: number, cols: number): PixelMetadata[][] {
+  const arr: PixelMetadata[][] = [];
+  for (let i = 0; i < rows; i++) {
+    const row: PixelMetadata[] = [];
+    for (let j = 0; j < cols; j++) {
+      row.push({ color: "#ffffff" });
+    }
+
+    arr.push(row);
+  }
+  return arr;
+}
+
+const width = 20;
+const height = 25;
+
+const CanvasComponent = (): JSX.Element => {
   const colors = [
     "white", // #ffffff
     // "rose-950",
@@ -23,106 +42,139 @@ const CanvasComponent: React.FC = () => {
   ];
 
   const canvasRef = useRef<HTMLDivElement>(null);
-  const appRef = useRef<PIXI.Application | null>(null);
-  const zoomContainerRef = useRef<PIXI.Container | null>(null);
-  const graphicsRef = useRef<PIXI.Graphics | null>(null);
-  const isDownRef = useRef(false);
-  const zoomedRef = useRef(false);
+  // const isDownRef = useRef(false);
+  // const zoomedRef = useRef(false);
 
-  const [grid, setGrid] = useState<{ [key: string]: { color: string } }>({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [pixelSize, setPixelSize] = useState(32);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [grid, setGrid] = useState<{ [key: string]: PixelMetadata }>({
     // Prepopulated pixel data as an example
     "5:5": { color: "#ff0000" },
-    "10:10": { color: "#00ff00" },
+    "10:7": { color: "#00ff00" },
     // Add more pixels as needed
   });
 
-  useEffect(() => {
-    // PIXI setup
-    const app = new PIXI.Application({ backgroundColor: 0xeeeeee });
-
-    const zoomContainer = new PIXI.Container();
-    app.stage.addChild(zoomContainer);
-    const graphics = new PIXI.Graphics();
-    zoomContainer.addChild(graphics);
-
-    // Assign to refs
-    appRef.current = app;
-    zoomContainerRef.current = zoomContainer;
-    graphicsRef.current = graphics;
-
-    // Initialize the color palette
-    const colors = ["#ffffff", "#ff0000", "#00ff00", "#0000ff", "#000000"];
-    const selectedColor = colors[0];
-
-    // Render initial grid
+  const canvasGrid = useMemo(() => {
+    const result = create2DArray(width, height);
     for (const pos in grid) {
-      renderPixel(pos, grid[pos]);
-    }
-
-    // Mouse events
-    app.stage.interactive = true;
-    app.stage.on("pointerdown", onDown);
-    app.stage.on("pointermove", onMove);
-    app.stage.on("pointerup", onUp);
-    app.stage.on("pointerupoutside", onUp);
-
-    function onDown(e: any) {
-      isDownRef.current = true;
-      const pos = getGridPos(e.data.global);
-      if (e.data.originalEvent.shiftKey) toggleZoom();
-      else placePixel(pos);
-    }
-
-    function onMove(e: any) {
-      if (!isDownRef.current) return;
-      const pos = getGridPos(e.data.global);
-      if (!e.data.originalEvent.shiftKey) placePixel(pos);
-    }
-
-    function onUp() {
-      isDownRef.current = false;
-    }
-
-    function placePixel(pos: string) {
-      const pixel = { color: selectedColor };
-      setGrid(prevGrid => ({ ...prevGrid, [pos]: pixel }));
-    }
-
-    function renderPixel(pos: string, pixel: { color: string }) {
       const [x, y] = pos.split(":").map(Number);
-      graphics.beginFill(parseInt(pixel.color.slice(1), 16));
-      graphics.drawRect(x * 10, y * 10, 10, 10);
-      graphics.endFill();
+      result[y][x] = grid[pos];
     }
+    return result;
+  }, [grid]);
 
-    function toggleZoom() {
-      zoomedRef.current = !zoomedRef.current;
-      zoomContainer.scale.set(zoomedRef.current ? 2 : 1);
-    }
-
-    function getGridPos(pos: PIXI.Point) {
-      const x = Math.floor(pos.x / 10);
-      const y = Math.floor(pos.y / 10);
-      return `${x}:${y}`;
-    }
-
-    const canvas: any = app.view;
-    canvasRef?.current?.appendChild(canvas);
-
-    return () => {
-      app.destroy(true, { children: true });
-    };
-  }, []);
+  // function renderPixel(g: any, pos: string, pixel: { color: string }) {
+  //   const [x, y] = pos.split(":").map(Number);
+  //   g.beginFill(parseInt(pixel.color.slice(1), 16));
+  //   g.drawRect(x * 10, y * 10, 10, 10);
+  //   g.endFill();
+  // }
 
   // useEffect(() => {
-  //   if (canvasRef.current) {
-  //     <PixiApp parentRef={canvasRef} />;
+  //   // PIXI setup
+  //   const app = new PIXI.Application({ backgroundColor: 0xeeeeee });
+
+  //   const zoomContainer = new PIXI.Container();
+  //   app.stage.addChild(zoomContainer);
+  //   const graphics = new PIXI.Graphics();
+  //   zoomContainer.addChild(graphics);
+
+  //   // Assign to refs
+  //   appRef.current = app;
+  //   zoomContainerRef.current = zoomContainer;
+  //   graphicsRef.current = graphics;
+
+  //   // Initialize the color palette
+  //   const colors = ["#ffffff", "#ff0000", "#00ff00", "#0000ff", "#000000"];
+  //   const selectedColor = colors[0];
+
+  //   // Render initial grid
+  //   for (const pos in grid) {
+  //     renderPixel(pos, grid[pos]);
   //   }
-  // }, []);
+
+  //   // Mouse events
+  //   app.stage.eventMode = "dynamic";
+  //   // app.stage.on("pointerdown", onDown);
+  //   app.stage.onpointerdown = onDown;
+  //   app.stage.on("pointermove", onMove);
+  //   app.stage.on("pointerup", onUp);
+  //   app.stage.on("pointerupoutside", onUp);
+
+  //   console.log(app.stage);
+
+  //   function onDown(e: any) {
+  //     console.log(e);
+  //     isDownRef.current = true;
+  //     const pos = getGridPos(e.data.global);
+  //     if (e.data.originalEvent.shiftKey) toggleZoom();
+  //     else placePixel(pos);
+  //   }
+
+  //   function onMove(e: any) {
+  //     if (!isDownRef.current) return;
+  //     const pos = getGridPos(e.data.global);
+  //     if (!e.data.originalEvent.shiftKey) placePixel(pos);
+  //   }
+
+  //   function onUp() {
+  //     isDownRef.current = false;
+  //   }
+
+  //   function placePixel(pos: string) {
+  //     const pixel = { color: selectedColor };
+  //     setGrid(prevGrid => ({ ...prevGrid, [pos]: pixel }));
+  //   }
+
+  //   function renderPixel(pos: string, pixel: { color: string }) {
+  //     const [x, y] = pos.split(":").map(Number);
+  //     graphics.beginFill(parseInt(pixel.color.slice(1), 16));
+  //     graphics.drawRect(x * 10, y * 10, 10, 10);
+  //     graphics.endFill();
+  //   }
+
+  //   function toggleZoom() {
+  //     zoomedRef.current = !zoomedRef.current;
+  //     zoomContainer.scale.set(zoomedRef.current ? 2 : 1);
+  //   }
+
+  //   function getGridPos(pos: PIXI.Point) {
+  //     const x = Math.floor(pos.x / 10);
+  //     const y = Math.floor(pos.y / 10);
+  //     return `${x}:${y}`;
+  //   }
+
+  //   const canvas: any = app.view;
+  //   if (canvasRef.current) {
+  //     canvasRef.current.appendChild(canvas);
+  //   } else {
+  //     console.error("Canvas ref not found");
+  //   }
+
+  //   return () => {
+  //     app.destroy(true, { children: true });
+  //   };
+  // }, [grid, canvasRef]);
 
   return (
-    <div className="h-screen bg-gray-300 overflow-hidden font-lato select-none">
-      <div id="canvas" ref={canvasRef} className="absolute top-0 bottom-16 left-0 right-0"></div>
+    <div className="h-screen bg-gray-300 overflow-hidden font-lato select-none flex items-center justify-center">
+      <div
+        id="canvas"
+        ref={canvasRef}
+        className={"absolute top-0 bottom-16 left-0 right-0 grid"}
+        style={{
+          gridTemplateColumns: `repeat(${height},${pixelSize}px)`,
+          gridTemplateRows: `repeat(${width},${pixelSize}px)`,
+        }}
+      >
+        {canvasGrid.map((row, i) => {
+          return row.map((pixel, j) => (
+            <div key={`${i}:${j}`} className="block w-full h-full" style={{ backgroundColor: pixel.color }}></div>
+          ));
+        })}
+      </div>
 
       <div className="controls absolute left-0 right-0 bottom-0 bg-gray-800 p-0 flex items-center justify-between overflow-hidden">
         <ul id="colors" className="m-0 p-2 flex-1 text-center transition-transform duration-500 ease-in-out relative">
