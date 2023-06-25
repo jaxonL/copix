3a29dd1902bfbca6fcd409c34edc947215b2a018
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2023-06-24 20:27:24 -0400

    chore: change to polygon mumbai for config

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -13,7 +13,9 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The network where your DApp lives in
-  targetNetwork: chains.hardhat,
+  // @dev: change this to test local/mumbai!!
+  // targetNetwork: chains.hardhat,
+  targetNetwork: chains.polygonMumbai,
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect on the local network
