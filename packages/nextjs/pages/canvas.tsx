69c589af00695199425d10885cee7724527e5069
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2023-06-25 03:35:01 -0400

    fix: read width, height from contract

--- packages/nextjs/pages/canvas.tsx
+++ packages/nextjs/pages/canvas.tsx
@@ -4,6 +4,8 @@ import Modal from "./paint_box";
 import SignInPrompt from "./sign_in_prompt";
 import { useAccount } from "wagmi";
 import { AuthContext } from "~~/components/copix/AuthContext";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { CONTRACT_NAME } from "~~/utils/constants";
 
 interface PixelMetadata {
   color: string;
@@ -14,7 +16,7 @@ interface PixelXY {
   y: number;
 }
 
-function create2DArray(rows: number, cols: number): PixelMetadata[][] {
+function create2DArray(cols: number, rows: number): PixelMetadata[][] {
   const arr: PixelMetadata[][] = [];
   for (let i = 0; i < rows; i++) {
     const row: PixelMetadata[] = [];
@@ -27,9 +29,6 @@ function create2DArray(rows: number, cols: number): PixelMetadata[][] {
   return arr;
 }
 
-const width = 20;
-const height = 25;
-
 const CanvasComponent = (): JSX.Element => {
   const [showModal, setShowModal] = useState(false);
   const [showSignIn, setShowSignIn] = useState(false);
@@ -37,6 +36,14 @@ const CanvasComponent = (): JSX.Element => {
   const { currentUser } = useContext(AuthContext);
   const [color, setColor] = useState<string>("#ffffff");
   const { address } = useAccount();
+  const { data: width, isLoading: loadingWidth } = useScaffoldContractRead({
+    contractName: CONTRACT_NAME,
+    functionName: "canvasWidth",
+  });
+  const { data: height, isLoading: loadingHeight } = useScaffoldContractRead({
+    contractName: CONTRACT_NAME,
+    functionName: "canvasHeight",
+  });
 
   const canvasRef = useRef<HTMLDivElement>(null);
   // const isDownRef = useRef(false);
@@ -57,13 +64,16 @@ const CanvasComponent = (): JSX.Element => {
    * actual array of pixels to render reading from grid
    */
   const canvasGrid = useMemo(() => {
-    const result = create2DArray(width, height);
+    if (!width || !height) {
+      return [];
+    }
+    const result = create2DArray(width.toNumber(), height.toNumber());
     for (const pos in grid) {
       const [x, y] = pos.split(":").map(Number);
       result[y][x] = grid[pos];
     }
     return result;
-  }, [grid]);
+  }, [grid, width, height]);
 
   function onPixelClicked(x: number, y: number) {
     if (!currentUser || !address) {
@@ -91,31 +101,31 @@ const CanvasComponent = (): JSX.Element => {
       <Modal {...selectedPixel} showModal={showModal} closeModal={closeModal} color={color} setColor={setColor} />
       <SignInPrompt showSignIn={showSignIn} closeSignIn={closeSignIn} />
       <div className="h-full bg-gray-300 overflow-hidden font-lato select-none flex items-center justify-center">
-        <div
-          id="canvas"
-          ref={canvasRef}
-          className={"grid"}
-          style={{
-            gridTemplateColumns: `repeat(${height},${pixelSize}px)`,
-            gridTemplateRows: `repeat(${width},${pixelSize}px)`,
-            border: "1px solid black",
-          }}
-        >
-          {canvasGrid.map((row, y) => {
-            return row.map((pixel, x) => (
-              <Pixel key={`${x}:${y}`} color={pixel.color} onClick={onPixelClicked.bind(null, x, y)} />
-            ));
-          })}
-        </div>
+        {loadingWidth || loadingHeight || !height || !width ? (
+          <div>Loading...</div>
+        ) : (
+          <div
+            id="canvas"
+            ref={canvasRef}
+            className={"grid"}
+            style={{
+              gridTemplateColumns: `repeat(${width.toNumber()},${pixelSize}px)`,
+              gridTemplateRows: `repeat(${height.toNumber()},${pixelSize}px)`,
+              border: "1px solid black",
+            }}
+          >
+            {canvasGrid.map((col, y) => {
+              return col.map((pixel, x) => (
+                <Pixel key={`${y}:${x}`} color={pixel.color} onClick={onPixelClicked.bind(null, x, y)} />
+              ));
+            })}
+          </div>
+        )}
 
         <div className="controls absolute left-0 right-0 bottom-0 bg-gray-800 p-0 flex items-center justify-between overflow-hidden">
-          {/* <ul id="colors" className="m-0 p-2 flex-1 text-center transition-transform duration-500 ease-in-out relative">
-            {colors.map(color => (
-              <li key={color} id={`c-${color}`} className={`w-6 h-6 inline-block list-none m-0 bg-${color} mr-1`}></li>
-            ))}
-          </ul> */}
-
-          <div className="face-space w-16"></div>
+          <div className="face-space w-16">
+            width: {width?.toNumber()}, height: {height?.toNumber()}
+          </div>
         </div>
       </div>
     </>
